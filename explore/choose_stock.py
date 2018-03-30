60c370abc9c8eef956beea36ebaea7b0899a8a34
zoobree <cugzhoubing@163.com>
2018-03-30 23:43:05 +0800

    1.o version

--- explore/choose_stock.py
+++ explore/choose_stock.py
@@ -17,10 +17,11 @@ DATA_PATH = "F:/mypython/tushare/data/"
 
 
 def decreasing_thr_judge(thr_value=0.2, str_stock_code="000651"):
-    df_one_stock_trade = ts.get_k_data(str_stock_code)
+    df_one_stock_trade = ts.get_k_data(code=str_stock_code, start='2012-06-30')
     try:
         close_price_min = np.min(df_one_stock_trade['close'])
         close_proce_cur = df_one_stock_trade['close'].values[-1]
+        print(df_one_stock_trade)
     except KeyError:
         return -1
     #print(close_proce_cur)
@@ -47,9 +48,13 @@ if __name__ == '__main__':
         stock_code_all_Array2=np.append(stock_code_all_Array2,decrease_value)
         #print(stock_code_all_Array2)
             #stock_code_all_DataFrame.append(Series_str_stock_code.)
-    df_all_stock_basics['decrease_value'] = pd.Series(stock_code_all_Array2)
+    print("***")
+    print(stock_code_all_Array2)
+    print(pd.Series(stock_code_all_Array2))
+    #df_all_stock_basics['decrease_value'] = pd.Series(stock_code_all_Array2)
+    df_all_stock_basics['decrease_value'] = stock_code_all_Array2
     df_all_stock_basics.to_csv(DATA_PATH+'decreasing_stock_info.csv',encoding='GBK')
     with open(DATA_PATH+'decreasing_stock_name.csv', 'wb') as file_handle:
-        np.savetxt(file_handle, stock_code_all_Array1, delimiter=",",fmt='%d')
+        np.savetxt(file_handle, stock_code_all_Array1, delimiter=",",fmt='s%')
     
     
